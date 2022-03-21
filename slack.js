71245780f42e4d54a5101773d94172c2db242327
임광훈 <hooeni@gmail.com>
2022-03-21 15:42:12 +0900

    key change 4

--- slack.js
+++ slack.js
@@ -1,7 +1,7 @@
 const Slack = require('slack-node');  // 슬랙 모듈 사용
 
 // apiToken = "xoxb-2187790532-3291583852624-iTA91CkKeJvKvqLkJAm2lXPk";
-apiToken = "xoxb-20949960309-3261241717542-nzII7OY6gPWUqmjqcQMNdO9H";
+apiToken = "xoxp-20949960309-20954168496-3280535436833-211a029dbb37ceb6fd2b966d12a80192";
 const slack = new Slack(apiToken);
 
 const send = async(message) => {
