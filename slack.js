a41ecb305266ea41944b627c5ec9acb8ca7ecdcf
임광훈 <hooeni@gmail.com>
2022-03-21 15:13:34 +0900

    test

---
+++ slack.js
@@ -0,0 +1,17 @@
+const Slack = require('slack-node');  // 슬랙 모듈 사용
+
+// apiToken = "xoxb-2187790532-3291583852624-iTA91CkKeJvKvqLkJAm2lXPk";
+apiToken = "xoxb-2187790532-3291583852624-iTA91CkKeJvKvqLkJAm2lXPk";
+const slack = new Slack(apiToken);
+
+const send = async(message) => {
+  slack.api('chat.postMessage', {
+	  username: 'test_bot',  // 슬랙에 표시될 봇이름
+	  text:message,
+	  channel:'#test'  // 전송될 채널 및 유저
+	}, function(err, response){
+	  console.log(response);
+	});
+}
+
+send('테스트 전송');
