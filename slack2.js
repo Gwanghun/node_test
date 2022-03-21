9ea41ad08bc8d68ebb7e6898ccc19eee01535551
임광훈 <hooeni@gmail.com>
2022-03-21 17:35:42 +0900

    SLACK_TOKEN change

--- slack2.js
+++ slack2.js
@@ -1,6 +1,7 @@
 const { WebClient } = require('@slack/web-api');
 
-apiToken = "xoxb-20949960309-3261241717542-0a2qUDJ7FHVcvWUaWgMlvteH";
+//apiToken = "xoxb-20949960309-3261241717542-0a2qUDJ7FHVcvWUaWgMlvteH";
+apiToken = process.env.SLACK_TOKEN
 const web = new WebClient(apiToken);
 // The current date
 const currentTime = new Date().toTimeString();
