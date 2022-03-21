082d586f89f52840c7d0b37b913a631134fc96ee
임광훈 <hooeni@gmail.com>
2022-03-21 15:34:58 +0900

    key change 2

--- slack.js
+++ slack.js
@@ -6,9 +6,9 @@ const slack = new Slack(apiToken);
 
 const send = async(message) => {
   slack.api('chat.postMessage', {
-	  username: 'test_bot',  // 슬랙에 표시될 봇이름
+	  username: 'hooeni_bot',  // 슬랙에 표시될 봇이름
 	  text:message,
-	  channel:'#test'  // 전송될 채널 및 유저
+	  channel:'#general'  // 전송될 채널 및 유저
 	}, function(err, response){
 	  console.log(response);
 	});
