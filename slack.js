06ea00f06cc48247ed6cf825d952b689c465b48e
임광훈 <hooeni@gmail.com>
2022-03-21 17:01:57 +0900

    nodejs slack message send

--- slack.js
+++ slack.js
@@ -1,7 +1,8 @@
 const Slack = require('slack-node');  // 슬랙 모듈 사용
 
 // apiToken = "xoxb-2187790532-3291583852624-iTA91CkKeJvKvqLkJAm2lXPk";
-apiToken = "xoxb-20949960309-3261241717542-f17ydBJr6wTvh9WZFcZkosfy";
+//apiToken = "xoxb-20949960309-3261241717542-MqWwuZObo5oInEON5edqgG4c";
+apiToken = "xoxp-20949960309-20954168496-3267934121122-d1dc4d37068e3d29652e60f118da5af1";
 const slack = new Slack(apiToken);
 
 const send = async(message) => {
@@ -11,7 +12,6 @@ const send = async(message) => {
 	  channel:'#test'  // 전송될 채널 및 유저
 	}, function(err, response){
 	  console.log(response);
-	  console.log(err);
 	});
 }
 
---
+++ slack2.js
@@ -0,0 +1,21 @@
+const { WebClient } = require('@slack/web-api');
+
+apiToken = "xoxb-20949960309-3261241717542-MqWwuZObo5oInEON5edqgG4c";
+const web = new WebClient(apiToken);
+// The current date
+const currentTime = new Date().toTimeString();
+
+(async () => {
+
+  try {
+    // Use the `chat.postMessage` method to send a message from this app
+    await web.chat.postMessage({
+      channel: '#oomool_dev',
+      text: `The current time is ${currentTime}`,
+    });
+    console.log('Message posted!');
+  } catch (error) {
+    console.log(error);
+  }
+
+})();
