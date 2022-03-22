b0f75ca9ca186d9ace02e1efbd3b0eaaee054912
임광훈 <hooeni@gmail.com>
2022-03-22 14:34:53 +0900

    token delete

--- slack.js
+++ slack.js
@@ -1,6 +1,5 @@
 const { WebClient } = require('@slack/web-api');
 
-//apiToken = "xoxb-20949960309-3261241717542-0a2qUDJ7FHVcvWUaWgMlvteH";
 apiToken = process.env.SLACK_TOKEN
 const web = new WebClient(apiToken);
 // The current date
